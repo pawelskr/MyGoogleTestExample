a282102df18586ee9e334181e2c7fa7862cf9461
Pawel <p.skrzypek@satrevolution.com>
2020-05-16 16:40:43 +0200

    Revert "init"
    This reverts commit 257b498aa905f6885ba5540dd5086ae9f35bf4c8.

--- build/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/pawel/Documents/DEVEL/GoogleTestFirst/googletest/googletest
-# Build directory: /Users/pawel/Documents/DEVEL/GoogleTestFirst/build/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
